748a7a9f106a78839f3a9097136716908e5340d9
rpopescu <popescu.roxana.andreea@gmail.com>
2015-11-25 15:11:27 +0200

    karma config

--- karma.conf.js
+++ karma.conf.js
@@ -8,22 +8,46 @@ module.exports = function(config){
         basePath: '',
 
 
-        // frameworks to use
-        // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-        frameworks: ['mocha', 'chai'],
 
+        // files to include, ordered by dependencies
+        files : [
+            // include relevant Angular files and libs
+            'app/lib/bower_components/angular/angular.js',
+            'app/lib/bower_components/angular/angular-mocks.js',
 
-        // list of files / patterns to load in the browser
-        files: [
-            './app/**/*.js',
-            './test/**/*.js'
+            // include js files
+            'app/js/app.js',
+
+            // include unit test specs
+            'test/unit/*.js'
         ],
+        // files to exclude
+        exclude : [
+             'app/lib/angular/*.min.js'
+        ],
+
+        // karma has its own autoWatch feature but Grunt watch can also do this
+        autoWatch : false,
+
+        // testing framework, be sure to install the karma plugin
+        frameworks: ['jasmine'],
+
+        // browsers to test against, be sure to install the correct karma browser launcher plugin
+        browsers : ['Chrome',  'Firefox'],
+
+        // progress is the default reporter
         reporters: ['progress'],
 
-        port:9876,
-        colors:true,
-        logLevel: config.LOG_INFO,
-        autoWatch: true,
-        singleRun: false
-    });
-};
+        // map of preprocessors that is used mostly for plugins
+        preprocessors: {
+
+        },
+
+        // list of karma plugins
+        plugins : [
+            'karma-junit-reporter',
+            'karma-chrome-launcher',
+            'karma-firefox-launcher',
+            'karma-jasmine'
+        ]
+    })}
